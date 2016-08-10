ff623e57437d31e790d620a35bc0b40ed69e0694
Mark John Buenconsejo <hi@markjeee.com>
2016-08-10 12:49:09 +0800

    Added Event scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Event.create!({ :title => 'Cebu City Marathon 2017',
+                :description => 'An annual tradition in Cebu, that is normally held one week before the Sinulog',
+                :organizer => 'Cebu Executive Runners Club',
+                :schedule => Time.parse('2017-01-08')
+              })
+
+Event.create!({ :title => 'Bohol International Marathon 2016',
+                :description => 'Run through the quiet and scenic road of Panglao Island.',
+                :organizer => 'Bohol Runners Club',
+                :schedule => Time.parse('2016-08-28')
+              })
